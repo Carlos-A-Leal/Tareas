386475d8b48bd42f458f9dee5d038abbf34710dd
Carlos-A-Leal <99233750+Carlos-A-Leal@users.noreply.github.com>
2022-02-07 22:02:02 -0600

    Add files via upload

---
+++ Actividad_2.js
@@ -0,0 +1,17 @@
+var mes = 10
+
+if(mes < 3)(
+console.log("es invierno")
+)else if(mes < 7)(
+console.log("es primavera")
+)else if(mes < 10)(
+console.log("es verano")
+)else if(mes < 12)(
+console.log("es otoño")
+)else if(mes < 13)(
+console.log("es invierno")
+)
+  
+if(mes = 10)(
+console.log("Carlos Andrés Leal González 22003771")
+)
